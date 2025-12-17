9a8d45beb7a5ac7fe99889f7330a7f72430dbacc
SonGitBukucu <makaya2620@gmail.com>
2025-12-17 17:30:48 +0300

    fix function

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,23 @@ int servoHesap(int, int, int);
 Servo parmak;
 void setup() {
   // put your setup code here, to run once:
-  //Serial.begin(9600);
+  Serial.begin(9600);
   parmak.attach(26);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int rawdeger = (constrain(analogRead(34), 0, 4096));
-  int deger = constrain(map(rawdeger, 0, 3300, 1000, 2000), 1000, 2000);
-  //Serial.println(deger);
-  parmak.writeMicroseconds(deger);
+  int rawdeger = (analogRead(34));
+  int deger = constrain(map(rawdeger, 2720, 4095, 1000, 2000), 1000, 2000);
+  //Serial.println(rawdeger);
+  Serial.println(deger);
+  //parmak.writeMicroseconds(deger);
 
 }
 
 // put function definitions here:
 int servoHesap(int pin, int altDeger, int ustDeger) {
-  int rawDeger = constrain(analogRead(pin), altDeger, ustDeger);
-  int deger = map(rawDeger, 0, 4096, 1000, 2000);
+  int rawDeger = analogRead(pin);
+  int deger = map(rawDeger, altDeger, ustDeger, 1000, 2000);
   return constrain(deger, 1000, 2000);
 }
